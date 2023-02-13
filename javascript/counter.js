2c72665b36781a161a0f5e6f388ec7ae9e0e2241
altinhysa <100517587+altinhysa@users.noreply.github.com>
2023-02-13 16:32:24 +0100

    counter numrat (#17)
    Co-authored-by: Taulant-Avdiu <taulantavdiu@gmail.com>

---
+++ javascript/counter.js
@@ -0,0 +1,41 @@
+const teamMembers = 93;
+const locations = 37;
+const awards = 74;
+const michelinStars = 31;
+
+let currentTeamMembers = 0;
+let currentLocations = 0;
+let currentAwards = 0;
+let currentMichelinStars = 0;
+
+const teamMembersCounter = setInterval(() => {
+    document.getElementById("teamMembers").innerHTML = currentTeamMembers;
+    currentTeamMembers++;
+    if (currentTeamMembers === teamMembers + 1) {
+        clearInterval(teamMembersCounter);
+    }
+}, 50);
+
+const locationsCounter = setInterval(() => {
+    document.getElementById("locations").innerHTML = currentLocations;
+    currentLocations++;
+    if (currentLocations === locations + 1) {
+        clearInterval(locationsCounter);
+    }
+}, 50);
+
+const awardsCounter = setInterval(() => {
+    document.getElementById("awards").innerHTML = currentAwards;
+    currentAwards++;
+    if (currentAwards === awards + 1) {
+        clearInterval(awardsCounter);
+    }
+}, 50);
+
+const michelinStarsCounter = setInterval(() => {
+    document.getElementById("michelinStars").innerHTML = currentMichelinStars;
+    currentMichelinStars++;
+    if (currentMichelinStars === michelinStars + 1) {
+        clearInterval(michelinStarsCounter);
+    }
+}, 50);
