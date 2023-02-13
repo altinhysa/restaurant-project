ea59b6fdc5789a21f134417fe2b0e7ad8100e49a
Altin Hysa <altinhysa84@gmail.com>
2023-02-13 22:35:08 +0100

    final changes

--- javascript/counter.js
+++ javascript/counter.js
@@ -1,6 +1,6 @@
-const teamMembers = 93;
-const locations = 37;
-const awards = 74;
+const teamMembers = 54;
+const locations = 10;
+const awards = 63;
 const michelinStars = 31;
 
 let currentTeamMembers = 0;
@@ -8,34 +8,51 @@ let currentLocations = 0;
 let currentAwards = 0;
 let currentMichelinStars = 0;
 
-const teamMembersCounter = setInterval(() => {
-    document.getElementById("teamMembers").innerHTML = currentTeamMembers;
-    currentTeamMembers++;
-    if (currentTeamMembers === teamMembers + 1) {
-        clearInterval(teamMembersCounter);
-    }
-}, 50);
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+            console.log(entry);
+            if(entry.isIntersecting){
+                const teamMembersCounter = setInterval(() => {
+                    document.getElementById("teamMembers").innerHTML = currentTeamMembers;
+                    currentTeamMembers++;
+                    if (currentTeamMembers === teamMembers) {
+                        clearInterval(teamMembersCounter);
+                    }
+                }, 50);
+                
+                const locationsCounter = setInterval(() => {
+                    document.getElementById("locations").innerHTML = currentLocations;
+                    currentLocations++;
+                    if (currentLocations === locations + 1) {
+                        clearInterval(locationsCounter);
+                    }
+                }, 50);
+                
+                const awardsCounter = setInterval(() => {
+                    document.getElementById("awards").innerHTML = currentAwards;
+                    currentAwards++;
+                    if (currentAwards === awards + 1) {
+                        clearInterval(awardsCounter);
+                    }
+                }, 50);
+                
+                const michelinStarsCounter = setInterval(() => {
+                    document.getElementById("michelinStars").innerHTML = currentMichelinStars;
+                    currentMichelinStars++;
+                    if (currentMichelinStars === michelinStars + 1) {
+                        clearInterval(michelinStarsCounter);
+                    }
+                }, 50);
+                
+            }else{
+                currentTeamMembers = 0;
+                currentLocations = 0;
+                currentAwards = 0;
+                currentMichelinStars = 0;
+            }
+    });
+});
 
-const locationsCounter = setInterval(() => {
-    document.getElementById("locations").innerHTML = currentLocations;
-    currentLocations++;
-    if (currentLocations === locations + 1) {
-        clearInterval(locationsCounter);
-    }
-}, 50);
 
-const awardsCounter = setInterval(() => {
-    document.getElementById("awards").innerHTML = currentAwards;
-    currentAwards++;
-    if (currentAwards === awards + 1) {
-        clearInterval(awardsCounter);
-    }
-}, 50);
-
-const michelinStarsCounter = setInterval(() => {
-    document.getElementById("michelinStars").innerHTML = currentMichelinStars;
-    currentMichelinStars++;
-    if (currentMichelinStars === michelinStars + 1) {
-        clearInterval(michelinStarsCounter);
-    }
-}, 50);
+const sectionFive = document.querySelector('.five-grid');
+observer2.observe(sectionFive);
